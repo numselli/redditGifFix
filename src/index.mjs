e3c5df3e2f14866db4de025c5de407b76a0375f0
numselli <58607232+numselli@users.noreply.github.com>
2022-04-06 15:05:19 -0700

    Update index.mjs

--- src/index.mjs
+++ src/index.mjs
@@ -14,19 +14,27 @@ const bot = new Eris(token, {
 bot.on("ready", ()=>{
     console.log("bot is ready")
 })
-bot.on("error", (error)=>{
-    console.log(error)
+bot.on("error", ()=>{
     console.log("an error has occurred")
 })
 
 // when a message is coreated
 bot.on("messageCreate", message=>{
+    // if the message is from a bot, end the function
     if (message.author.bot) return;
+
+    // if the message has a reddit link
     if (message.content.includes("preview.redd.it")){
+        // replace preview with i
         const prams = message.content.replace("preview.redd.it", "i.redd.it").split("?")
+        
+        // take out anything after the ? in the url
         const anythingElse = prams[1].split(" ")
         anythingElse.shift()
+        
+        // send a message with the original message content and the modified url to the channel
         message.channel.createMessage(`${prams[0]} ${anythingElse.join(" ")}`)
+        .catch(error=>{})
     }
 })
 
